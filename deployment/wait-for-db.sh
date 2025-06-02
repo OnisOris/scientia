d3f8b04147d8f1b55c80b8ce75ec34ca84441736
Michael <onisoris@yandex.ru>
2025-06-02 15:15:51 +0300

    Fix: problems in installing the scientia at container

---
+++ deployment/wait-for-db.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=$DB_PASS psql -h "$host" -U $DB_USER -d $DB_NAME -c '\q'; do
+  >&2 echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "PostgreSQL is up - executing command"
+exec $cmd
