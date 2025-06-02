d32bdc2561836125f987b369d34ec04c141e031e
Michael <onisoris@yandex.ru>
2025-06-02 17:46:25 +0300

    Fix: error in Dockerfile and docker-compose.yaml

--- deployment/wait-for-db.sh
+++ deployment/wait-for-db.sh
@@ -1,15 +1,16 @@
+#!/bin/s
 #!/bin/sh
 
 set -e
 
 host="$1"
 shift
-cmd="$@"
 
-until PGPASSWORD=$DB_PASS psql -h "$host" -U $DB_USER -d $DB_NAME -c '\q'; do
+until PGPASSWORD=$DB_PASS psql -h "$host" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
   >&2 echo "PostgreSQL is unavailable - sleeping"
   sleep 1
 done
 
 >&2 echo "PostgreSQL is up - executing command"
-exec $cmd
+exec "$@"
+
